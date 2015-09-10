749b660eba2e8966507b4f62cca28b9beff5c77d
Syed sehu <getsyed.a@gmail.com>
2015-09-10 23:11:41 +0530

    Adding overlay menu design changes and icons for it

--- app/src/main/java/com/fyshadows/beware/Home.java
+++ app/src/main/java/com/fyshadows/beware/Home.java
@@ -118,7 +118,14 @@ public class Home extends AppCompatActivity {
         btnMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MenuLayout.setVisibility(View.VISIBLE);
+                if(MenuLayout.getVisibility() == View.VISIBLE) {
+                    MenuLayout.setVisibility(View.GONE);
+                    btnMenu.setImageResource(R.drawable.menu);
+                }
+                else {
+                    MenuLayout.setVisibility(View.VISIBLE);
+                    btnMenu.setImageResource(R.drawable.menuclose);
+                }
             }
         });
 
@@ -126,6 +133,7 @@ public class Home extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 MenuLayout.setVisibility(View.INVISIBLE);
+                btnMenu.setImageResource(R.drawable.menu);
             }
         });
 
@@ -143,6 +151,7 @@ public class Home extends AppCompatActivity {
         btnMyPost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                btnMenu.setImageResource(R.drawable.menu);
                 txtactionbar.setText("My Post");
                 btnHome.setVisibility(View.VISIBLE);
                 MenuLayout.setVisibility(View.INVISIBLE);
